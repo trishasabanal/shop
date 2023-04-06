66e21f32a98982aa863911e131fd273024287a36
unknown <tsabanal@gbox.adnu.edu.ph>
2023-04-07 00:35:22 +0800

    Third Commit

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,8 @@ Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/shop', [App\Http\Controllers\ProductController::class, 'index'])->name('shop');
+Route::get('/shop/add', [App\Http\Controllers\ProductController::class, 'create'])->name('add');
+Route::post('/shop/store', [App\Http\Controllers\ProductController::class, 'store'])->name('store');
+Route::get('/shop/edit/{id}', [App\Http\Controllers\ProductController::class, 'edit'])->name('edit');
+Route::patch('/shop/update/{id}', [App\Http\Controllers\ProductController::class, 'update'])->name('update');
+Route::delete('/shop/delete/{id}', [App\Http\Controllers\ProductController::class, 'destroy'])->name('delete');
